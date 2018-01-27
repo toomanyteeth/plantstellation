2dd0d8df2f210651c5cffb6dccc793bddeb6a0ff
Meena Vempaty <meena@through-the-floor.com>
2018-01-27 14:59:27 -0800

    beams are drawn between objects

---
+++ unity/Assets/Scripts/MakeBeams.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBeams : MonoBehaviour {
+
+	float[,] distances;
+	public GameObject beamPrefab;
+	public float maxBeamDistance;
+	private GameObject[,] beams;
+	private GameObject[] objects;
+
+	// Use this for initialization
+	void Start () {
+		GameObject main = GameObject.Find("Main");
+		ObjectDistanceCalculator odc = main.GetComponent<ObjectDistanceCalculator>();
+		distances = odc.distances;
+		beams = new GameObject[distances.GetLength(0), distances.GetLength(1)];
+		objects = odc.objects;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < distances.GetLength(0); i++) {
+			for(int j = 0; j < distances.GetLength(1); j++) {
+				if(distances[i, j] != null && distances[i, j] > 0 && distances[i, j] < maxBeamDistance){
+					if(beams[i, j] == null) {
+						beams[i, j] = Instantiate(beamPrefab, transform);
+					}
+					beams[i, j].GetComponent<LineRenderer>().SetPositions(new Vector3[] {objects[i].transform.position, objects[j].transform.position});
+				} else if (beams[i, j] != null) {
+					Destroy(beams[i, j]);
+				}
+			}
+		}
+	}
+}
---
+++ unity/Assets/Scripts/ObjectDistanceCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDistanceCalculator : MonoBehaviour {
+
+static int NUM_OBJECTS = 7;
+
+public GameObject[] targets = new GameObject[NUM_OBJECTS];
+bool[] objectInScene = new bool[NUM_OBJECTS];
+
+public GameObject[] objects = new GameObject[NUM_OBJECTS];
+public float[,] distances = new float[NUM_OBJECTS, NUM_OBJECTS];
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < NUM_OBJECTS; i++) {
+			for(int j = 0; j < NUM_OBJECTS; j++) {
+				if(objects[i] != null && objects[j] != null){
+					distances[i, j] = Vector3.Distance(objects[i].transform.position, objects[j].transform.position);
+				}
+			}
+		}
+	}
+}
