a2383084738bab4f0528c9c04c39e60526ccfd70
Meena Vempaty <meena@through-the-floor.com>
2018-01-28 13:53:26 -0800

    particles in beams

--- unity/Assets/Scripts/Beam.cs
+++ unity/Assets/Scripts/Beam.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Beam : MonoBehaviour {
 
+	Vector3 startPoint = Vector3.zero;
+	Vector3 endPoint = Vector3.zero;
+	GameObject particleStopper;
+
 	// Use this for initialization
 	void Start () {
-		
+		particleStopper = gameObject.transform.GetChild(0).gameObject;
+		UpdateBeam(startPoint, endPoint);
 	}
-	
+
+	public void UpdateBeam(Vector3 newStartPoint, Vector3 newEndPoint) {
+		startPoint = newStartPoint + Vector3.up*0.1f;
+		endPoint = newEndPoint + Vector3.up*0.1f;
+		transform.position = startPoint;
+		transform.rotation = Quaternion.LookRotation(endPoint - startPoint);
+		if(particleStopper) {
+			particleStopper.transform.position = endPoint;
+		}
+		gameObject.GetComponent<LineRenderer>().SetPositions(new Vector3[] {startPoint, endPoint});
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
---
+++ unity/Assets/Scripts/BeamManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamManager : MonoBehaviour {
+
+	private ObjectManager objectManager;
+	public GameObject beamPrefab;
+	public float maxBeamDistance;
+	private GameObject[,] beams;
+
+	// Use this for initialization
+	void Start () {
+		GameObject main = GameObject.Find("Main");
+		objectManager = main.GetComponent<ObjectManager>();
+		beams = new GameObject[objectManager.objects.Length, objectManager.objects.Length];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < objectManager.objects.Length; i++) {
+			for(int j = 0; j < objectManager.objects.Length; j++) {
+				if(objectManager.distances[i, j] != null && objectManager.distances[i, j] > 0 && objectManager.distances[i, j] < maxBeamDistance){
+					if(!beams[i, j]) {
+						beams[i, j] = Instantiate(beamPrefab, transform);
+					}
+					beams[i, j].GetComponent<Beam>().UpdateBeam(objectManager.objects[i].transform.position, objectManager.objects[j].transform.position);
+				} else if (beams[i, j]) {
+					Destroy(beams[i, j]);
+				}
+			}
+		}
+	}
+}
