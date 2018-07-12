a67dc5054903e338d24084c975c7e1181b3d656b
Meena Vempaty <meenamakesthings@gmail.com>
2018-07-12 11:22:16 -0700

    UI updated

---
+++ unity/Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctions : MonoBehaviour {
+
+	public void OpenURL (string url) {
+		Application.OpenURL(url);
+	}
+
+}
